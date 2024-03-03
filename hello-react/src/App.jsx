b920cafdae0a50d1937450521730f83fce7f94b1
kihan2518B <kp648027@gmail.com>
2024-03-03 23:09:36 +0530

    showing data dynamically using props intaskCard component

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,12 +1,39 @@
 // import { useState } from 'react'
-
-import './App.css'
+import './App.css';
+import TaskCards from './components/taskCards';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div>Hello world</div>
+    <div className=' font-serif bg-red-100'>
+      <div className='pt-10 ml-20'>
+        <h1 className='font-bold  '>Smarter Tasks</h1>
+        <h5 className='flex'>Project:<p className='ml-2'>Final year project</p></h5>
+      </div>
+
+      <div className="flex ml-10 mt-16 h-[70vh] w-[90vw] justify-center gap-8">
+        <div className='w-[40vw] h-fit flex flex-col border-4 rounded-3xl border-red-500 items-center gap-5'>
+          <div className='w-full flex justify-center mt-3'>
+            <h1 className='font-bold text-3xl'>Pending</h1>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-4 pb-4'>
+            <TaskCards title={"Build Website with static content"} completed={false} dueDate={"7th April"} assigneeName={"Rohit S"} />
+            <TaskCards title={"Add a blog"} completed={false} dueDate={"10th April"} assigneeName={"Mehul A"} />
+          </div>
+        </div>
+
+        <div className='w-[40vw] h-fit flex flex-col border-4 rounded-3xl border-red-500 items-center gap-5'>
+          <div className='w-full flex justify-center mt-3'>
+            <h1 className='font-bold text-3xl'>Done</h1>
+          </div>
+          <div className='flex flex-col pb-4 gap-4 justify-center items-center '>
+            <TaskCards title={"Design a mockup"} completed={true} dueDate={"20th April"} completedAtDate={"2th April"} assigneeName={"Rohit M"} />
+            <TaskCards title={"Get approval from principal"} completed={true} dueDate={"1th April"} completedAtDate={"31th March"} assigneeName={"Mohan M"} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
