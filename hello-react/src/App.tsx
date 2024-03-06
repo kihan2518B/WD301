d91a9166d18090941e26c9ad4f46887689b73015
kihan2518B <kp648027@gmail.com>
2024-03-06 21:25:32 +0530

    added interface to TaskCard

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,8 +1,9 @@
 // import { useState } from 'react'
+import React from 'react';
 import './App.css';
 import TaskCard from './TaskCard'; // eslint-disable-line
 
-function App() {
+const App: React.FC = () => {
   // const [count, setCount] = useState(0)
 
   return (
---
+++ hello-react/src/TaskCard.tsx
@@ -0,0 +1,44 @@
+/*  eslint-disable react/prop-types */
+import React from "react";
+import "./TaskCard.css";
+
+interface TaskCardProps {
+    title: string;
+    completed: boolean;
+    assigneeName: string;
+    dueDate: string;
+    completedAtDate?: string; //makes it optional
+}
+
+const TaskCard: React.FC<TaskCardProps> = (props) => {
+    //logic for Date Element
+    let Date = props.dueDate;
+    const Assignee = props.assigneeName;
+    let title = props.title;
+    let DateElement: string;
+    if (props.completed) {
+        DateElement = "completed on: " + props.completedAtDate;
+    }
+    else {
+        DateElement = "due date: " + Date;
+    }
+
+    return (
+        <div className="w-[35vw] h-fit border-2 border-gray-900 px-5 py-5 rounded-lg">
+            <div>
+                <h4 className="text-xl font-serif font-bold text-red-500">{title}</h4>
+            </div>
+            <div className="">
+                <p>{DateElement}</p>
+            </div>
+            <div className="assignee">
+                <h4>Assignee name:{Assignee}</h4>
+
+            </div>
+        </div>
+    )
+}
+
+/* eslint-disable react/prop-types */
+
+export default TaskCard;
