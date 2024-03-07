489a17e17a99999cec565d72674125b34ed4908f
kihan2518B <kp648027@gmail.com>
2024-03-07 19:55:26 +0530

    Created Smarter-tasks and initialized basic things

---
+++ smarter-tasks/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Task from './Task'
+
+function App() {
+
+  return (
+    <div className='text-red-400 font-bold flex items-center justify-center'>
+      <Task />
+    </div>
+  )
+}
+
+export default App
---
+++ smarter-tasks/src/Task.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Task extends React.Component {
+    render() {
+        return <div> Buy Groceries!!</div>
+    }
+}
+
+export default Task
