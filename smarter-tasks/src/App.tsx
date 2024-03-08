131185ec0bcce506066225603c99e3cb54a38873
kihan2518B <kp648027@gmail.com>
2024-03-08 11:34:46 +0530

    created clock using state and life cycle methods

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
-import Task from './Task'
+import Task from './Task';
+import Clock from './Clock';
 
 function App() {
 
   return (
     <div className='text-red-400 font-bold flex items-center justify-center'>
       <Task />
+      <Clock />
     </div>
   )
 }
