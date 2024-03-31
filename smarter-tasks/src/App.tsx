ead406de90b3891d02e04b0815f2bc6bba827bff
kihan2518B <kp648027@gmail.com>
2024-04-01 00:15:25 +0530

    Created pages and route for home page and tasks page

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,12 +1,26 @@
 import './App.css'
-import TaskApp from './TaskApp';
+// import TaskApp from './TaskApp';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
+import TaskListPage from './pages/TaskListPage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/tasks',
+      element: <TaskListPage />
+    },
+  ])
 
   return (
     <div className='text-red-400 font-bold flex items-center justify-center'>
-      <TaskApp />
+      <RouterProvider router={router} />
+      {/* <TaskApp /> */}
     </div>
   )
 }
